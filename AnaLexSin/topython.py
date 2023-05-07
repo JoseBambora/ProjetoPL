f0e051dd4fd2179d512d9a2d54abe7eeefaec7a5
JoseBambora <zebragapt@gmail.com>
2023-05-07 19:32:09 +0100

    Simplified the code on topython.py

--- AnaLexSin/topython.py
+++ AnaLexSin/topython.py
@@ -4,94 +4,27 @@ import re
 import numpy as np
 exp = re.compile(r'((\+|\-)?\d+(\.\d+)?|True|False)')
 
-# Argumentos (Argumento => string)
-def takeout_lists_args(arg):
-    if isinstance(arg,ListStatic) or isinstance(arg,ListVar):
-        res = (arg.getName(),arg.toPythonArgs(),arg.toPythonIf())
-    elif isinstance(arg,list):
-        name = ''.join(arg)
-        res = (name,'','')
-    else:
-        res = (arg,'','')
-    return res
-
-# Argumentos:
-# Converter todos os argumentos em string e buscar pedaços de código em 
-# relação ao pattern-matching e associação de variaives, ambos para listas
-def get_args_ifs_codigo(implementacao):
-    argumentos = implementacao['args']
-    if(len(argumentos) == 0):
-        args = []
-        codigo = []
-        ifs = []
-    else:
-        argsaux = list(map(lambda a: takeout_lists_args(a),argumentos))
-        args = list(map(lambda a: a[0],argsaux))
-        codigo = list(filter(lambda s: s != '',map(lambda a: a[1],argsaux)))
-        ifs = list(filter(lambda s: s != '', map(lambda a: a[2],argsaux)))
-    return (args,' and '.join(ifs),'\n'.join(codigo))
-
-# Associação de variaveis + pattern matching
-# Associação de variaveis e pattern matching para números e bools 
-def trata_argumentos(argumentos,args):
-    # criar condição
-    condicoes = []
-    codigo = []
-    index = 0
-    for a in args:
-        if exp.match(a):
-            condicoes.append(f'{argumentos[index]} == {a}')
-        index += 1
-    # associar variaveis
-    aux = {}
-    index = 0
-    for a in args:
-        if not exp.match(a):
-            codigo.append(f'\t\t{a} = {argumentos[index]}')
-            aux[a] = argumentos[index]
-        index += 1
-    return (' and '.join(condicoes), '\n'.join(codigo),aux)
-
 # Argumentos: nomes dos argumentos
 def geraArgs(argumentos):
     return list(map(lambda num: f'arg{num}',range(0,len(argumentos))))
 
-# Pattern-Matching (pm) 
-# juntar pm das listas (get_args_ifs_codigo) + números / bools (trata_argumentos)
-def gera_if(implementacoes, ifsargs, ifs,aux):
-    strif = ''
-    if ifsargs != '':
-        strif = ifsargs
-    if ifs != '':
-        if ifsargs != '':
-            strif += ' and '
-        strif += ifs
-    if strif != '':
-        res = f'\tif {strif}:'
-    elif(len(implementacoes) > 1):
-        res = f'\telse:'
-    else:
-        res = ''
-    for a in aux.keys():
-        res = re.sub(f'\({a}',f'({aux[a]}',res)
-    return res
-
 # Passar o else para última posição
+# tirar 1 tab se a função não tiver pattern-matching
 def reorganiza_fun(codigofun):
     elseCorpo = []
     resto = []
-    b = False
+    insideelse = False
     haveIfs = re.match(r'(\tif|\telse)',codigofun[1])
     if haveIfs:
         haselse = False
         for s in codigofun:
             if '\telse:' == s:
                 haselse = True
-                b = True
+                insideelse = True
                 elseCorpo.append(s)
-            elif b:
+            elif insideelse:
                 if re.match(r'\tif',s):
-                    b = False
+                    insideelse = False
                     resto.append(s)
                 else:
                     elseCorpo.append(s)
@@ -106,6 +39,59 @@ def reorganiza_fun(codigofun):
         resto.insert(0,codigofun[0])
     return resto + elseCorpo
 
+
+def pattern_matching_elem(arg,word):
+    if isinstance(arg,ListStatic) or isinstance(arg,ListVar):
+        arg.setName(word)
+        res = arg.toPythonIf()
+    else:
+        if isinstance(arg,list):
+            arg = ''.join(arg)
+        if exp.match(arg):
+            res = f'{word} == {arg}'
+        else:
+            res = ''
+    return res
+
+def associacao_vars_elem(arg,word):
+    if isinstance(arg,ListStatic) or isinstance(arg,ListVar):
+        arg.setName(word)
+        res = arg.toPythonArgs()
+    else:
+        if isinstance(arg,list):
+            arg = ''.join(arg)
+        if not exp.match(arg):
+            res = f'\t\t{arg} = {word}'
+        else:
+            res = ''
+    return res
+
+def aux_pm_av(argumentos,args,funcao):
+    res = []
+    for index in range(0,len(args)):
+        resaux = funcao(args[index],argumentos[index])
+        if len(resaux) > 0:
+            res.append(resaux)
+    return res
+
+def pattern_matching(argumentos,args):
+    condicoes = aux_pm_av(argumentos,args,pattern_matching_elem)
+    strfinal = ''
+    if(len(condicoes) > 0):
+        strfinal = ' and '.join(condicoes)
+        strfinal = f'\tif {strfinal}:'
+    return strfinal
+
+def associacao_vars(argumentos,args):
+    associacao = aux_pm_av(argumentos,args,associacao_vars_elem)
+    strfinal = ''
+    if(len(associacao) > 0):
+        strfinal = '\n'.join(associacao)
+    return strfinal
+
+def gera_codigo(resultado):
+    return str(resultado)
+
 # Corpo função: to python de 1 função
 def toPythonFun(fun):
     name = fun['nome']
@@ -114,17 +100,16 @@ def toPythonFun(fun):
     aux = ','.join(argumentos)
     codigoFun = [f'def {name}({aux}):']
     for i in implementacoes:
-        args, ifs,codigo = get_args_ifs_codigo(i)
-        ifsargs, codigoargs, aux = trata_argumentos(argumentos,args)
-        se = gera_if(implementacoes,ifsargs,ifs,aux)
-        if se !=  '':
-            codigoFun.append(se)
-        if len(codigoargs) > 0:
-            codigoFun.append(codigoargs)
-        if len(codigo) > 0:
-            codigoFun.append(codigo)
-        resultado = str(i['res'])
-        codigoFun.append(resultado)
+        pm = pattern_matching(argumentos,i['args'])
+        av = associacao_vars(argumentos,i['args'])
+        gc = gera_codigo(i['res'])
+        if len(pm) > 0:
+            codigoFun.append(pm)
+        elif len(implementacoes) > 1:
+            codigoFun.append('\telse:')
+        if len(av) > 0:
+            codigoFun.append(av)
+        codigoFun.append(gc)
     return reorganiza_fun(codigoFun)
 
 def toPython(parser,namefileres):
